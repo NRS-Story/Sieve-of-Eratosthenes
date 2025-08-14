0edcb1bd5a210fadb071e21e2f7edf2764c70ee6
NRS-Story <nafijurrahmansadi04@yahoo.com>
2025-08-14 12:47:11 +0600

    Create Optimized-Range-Prime-Sieve.cpp

---
+++ Optimized-Range-Prime-Sieve.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+const int MOD = 1e9 + 7;
+
+ll n;
+vector<bool> prime;
+
+void sieve(ll n)
+{
+    prime.assign(n + 1, true);
+    prime[0] = prime[1] = false;
+
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (prime[i])
+        {
+            for (ll j = i * i; j <= n; j += i)
+                prime[j] = false;
+        }
+    }
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+
+    sieve(n);
+
+    for (ll i = 2; i <= n; i++)
+    {
+        if (prime[i])
+            cout << i << ' ';
+    }
+    cout << '\n';
+}
